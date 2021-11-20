d3a427df2350850d4f823bba08dcc2f5e61b9efc
JulesOfAllTrades <jules.marie.david@gmail.com>
2021-11-19 21:28:05 -0500

    button and text styling, added more name options

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react';
+import './App.css';
+const adjectives = ["Lilac", "Scary", "Enormous", "Blind", "Hopeful", "Warped", "Vintage", "Great", "Terrible", "Dental", "Filthy", "Brainy", "Cosmic", 
+                    "Discretionary", "Slushy", "Opaque", "Wet", "Conspiratorial", "Aluminum", "Brave", "Heartbroken", "Miserable", "Holy", "Plucky", "Sympathetic", "Brass"] 
+const nouns = ["Octopi", "Happiness", "Shrubbery", "Bracelets", "Code", "Soap", "Cans", "Messages", "Lighter", "Bass", "Puppies", "Courts", "Pages", "Gasps", "Pendulum",
+                "Film", "Bigfoot", "Sons", "Stopwatch", "Cartographer", "Plothole", "Error", "Bass", "Bottle", "Engine", "Landmine", "Sumbarine", "Drugs"]
+function GenerateBandName() {
+
+  function randomAjective() {
+    return adjectives[(Math.floor(Math.random() * adjectives.length))]
+
+}
+
+function randomNoun() {
+    return nouns[(Math.floor(Math.random() * nouns.length))]
+
+}
+    return `Hey, we should call the band The ${randomAjective() +  ' ' + randomNoun()}.`
+}
+
+
+const App = () => {
+  const [message, setMessage] = useState()
+  const handleClick = (e) => {
+    e.preventDefault()
+    setMessage(GenerateBandName())
+  } 
+console.log(message, 'message')
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button className="App-button" type="button" id="button" onClick={handleClick} > What should we call the band?</button>
+      <p className="App-message">
+        {message}
+      </p>
       </header>
     </div>
   );
